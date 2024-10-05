fec62867744371a8937b6325464f98340914bad4
Sebastuancruz <sebastian_rodas26@outlook.es>
2024-10-05 13:32:20 -0600

    delete github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site:'https://Sebastuancruz.github.io',
-  base:'sebastian-portfolio'
+  // site:'https://Sebastuancruz.github.io',
+  // base:'sebastian-portfolio'
 });
